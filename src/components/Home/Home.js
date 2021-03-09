bdaaa9567bcbf96aa27da85aec092abfbc0617de
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-09 13:12:14 +0100

    Get and keep username, and passwords input values

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home">Holi</div>;
+  return (
+    <div className="home">
+      <img src="../../images/devGirl.png" alt="imagen" />
+    </div>
+  );
 };
 Home.propTypes = {};
 export default Home;
--- src/components/SingUp/SingUp.js
+++ src/components/SingUp/SingUp.js
@@ -3,21 +3,46 @@ import "./SingUp.css";
 import { Link } from "react-router-dom";
 
 const SingUp = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+
   return (
     <div className="container">
       <form action="" className="form">
+        <img src="../../images/devGirl.png" alt="Dev Girl" />
         <label htmlFor="userName" className="name">
           Name
         </label>
-        <input type="text" className="input name" />
+        <input
+          type="text"
+          className="input name"
+          onChange={(ev) => {
+            setUserName(ev.target.value);
+          }}
+        />
         <label htmlFor="password" className="password">
           Password
         </label>
-        <input type="password" name="password" className="input" />
-        <label htmlFor="passwordConfirm" className="password">
-          Confirm Password
+        <input
+          type="password"
+          name="password"
+          className="input"
+          onChange={(ev) => {
+            setPassword(ev.target.value);
+          }}
+        />
+        <label htmlFor="passwordConfirmed" className="password">
+          Confirmed Password
         </label>
-        <input type="password" name="passwordConfirm" className="input" />
+        <input
+          type="password"
+          name="passwordConfirmed"
+          className="input"
+          onChange={(ev) => {
+            setConfirmedPassword(ev.target.value);
+          }}
+        />
         <Link to="/home">
           <input type="button" value="Sign Up" className="signUpButton" />
         </Link>
