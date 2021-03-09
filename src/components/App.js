9054119f099b819fcd4d4e28cf85819956f8bf3e
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-09 21:39:18 +0100

    Run docs folder to production

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import SignUp from "./SignUp/SignUp";
 import Home from "./Home/Home";
