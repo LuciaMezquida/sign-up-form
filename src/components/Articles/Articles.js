f9634a3e8abccf6b679932d765fdfdb4503ba90d
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-15 14:41:17 +0100

    Create Articles component and migrate article from Home component

---
+++ src/components/Articles/Articles.js
@@ -0,0 +1,34 @@
+import "./Articles.css";
+
+const Articles = () => {
+  return (
+    <article className="article">
+      <h3 className="article-title">We are hiring!</h3>
+      <p className="hiring">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, totam
+        necessitatibus. Amet earum voluptate, magnam veritatis numquam dolores
+        esse doloribus expedita, minus perferendis unde eligendi aliquid culpa
+        nemo totam error. Earum, tempore voluptatibus delectus sit cum ipsam
+        explicabo exercitationem facilis ipsum unde, inventore, consectetur nam
+        error architecto obcaecati quos temporibus rem doloremque. Itaque id et
+        optio. Ea dolor exercitationem expedita! Nobis eligendi cumque rerum
+        quos nam itaque magnam iste, hic saepe libero, labore quia. Distinctio
+        cupiditate architecto ex aut ad beatae nulla quis, aperiam ullam natus
+        quam labore obcaecati magni. Assumenda expedita culpa ullam harum, unde
+        voluptatem dolor ipsa consectetur libero laudantium nostrum hic amet! In
+        sapiente, laudantium accusantium vel et soluta quasi tenetur error a
+        vero, eius vitae nihil. Autem modi sequi eaque voluptate est consectetur
+        soluta voluptatibus illo ex labore consequuntur odit minus quas quis
+        possimus facilis, at voluptatum totam maiores tempore nostrum! Ex a
+        harum animi blanditiis. Pariatur, nisi quas? Odio, quis neque! Modi
+        harum repellat consequuntur praesentium magnam reprehenderit eum dolores
+        fugiat dolorem! Ad asperiores blanditiis, perferendis dignissimos
+        repellat totam, dolor quasi quo ab soluta veritatis. Enim eaque qui
+        impedit aspernatur corporis et temporibus accusantium blanditiis nihil
+        porro nesciunt corrupti amet ullam eius quo laudantium reprehenderit
+        dolor mollitia alias, voluptatem voluptas earum?
+      </p>
+    </article>
+  );
+};
+export default Articles;
